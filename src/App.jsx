fe9b51170369a50298b0edc19a22e2537706e365
ervanervan <ervanervan040@gmail.com>
2024-09-18 11:03:40 +0700

    finish: slider animation

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import wallpaper4 from "./assets/wallpaper 4.jpg";
 import CharacterCard from "./component/CharacterCard";
 
 function App() {
-  // Dummy data for the characters
   const characters = [
     {
       id: 1,
